782318ca7757d3150019568b1f93d9f373f57446
huxinguang <1259198268@qq.com>
2019-04-02 10:42:43 +0800

    fix

---
+++ VideoMaterial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoMaterial
+//
+//  Created by xinguang hu on 2019/4/2.
+//  Copyright © 2019 Yunbangshou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
